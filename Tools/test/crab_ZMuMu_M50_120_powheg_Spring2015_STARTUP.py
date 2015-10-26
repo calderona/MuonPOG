06b6bbb82736624dbf895fe78ce685ee1571b451
Federica <fedespring@gmail.com>
2015-10-26 16:44:37 +0100

    added Startup cfg

--- Tools/test/crab_ZMuMu_M50_120_powheg_Spring2015_STARTUP.py
+++ Tools/test/crab_ZMuMu_M50_120_powheg_Spring2015_STARTUP.py
@@ -6,7 +6,7 @@ config.General.workArea = 'NTuples_ZMuMu_M50_120_STARTUP'
 config.General.transferLogs = True
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'muonPogCrabNtuplesMC_cfg.py'   
+config.JobType.psetName = 'muonPogCrabNtuplesStartupMC_cfg.py'   
 config.JobType.priority = 1
 
 config.Data.inputDataset = '/ZToMuMu_NNPDF30_13TeV-powheg_M_50_120/RunIISpring15DR74-Startup_EXOReReco_74X_Spring15_mcRun2_startup_v0-v1/AODSIM'
