06812d3e4ac1d84383c8f92933c0bfcaa2015195
Carlo Battilana <carlo.battilana@gmail.com>
2016-01-05 12:13:11 +0100

    Added cosmics, some fixes

--- Tools/test/das/reRecoSummary.sh
+++ Tools/test/das/reRecoSummary.sh
@@ -2,7 +2,8 @@
 
 eval `scramv1 runtime -sh`
 
-das_client.py --limit=0 --query="dataset=/*Mu*/*16Dec2015*/AOD*" &> data_all.txt
+das_client.py --limit=0 --query="dataset dataset=/*Mu*/*16Dec2015*/AOD* "  &> data_all.txt
+das_client.py --limit=0 --query="dataset dataset=/*Cosm*/*/* release=CMSSW_7_6_*"  &> cosmics_all.txt
 das_client.py --limit=0 --query="dataset=/*/*DR76*/AOD*" &> mc_all.txt
 
 echo "[$0] Status of SingleMuon data reprocessing:" 
@@ -15,8 +16,13 @@ cat data_all.txt | grep Double
 echo
 echo
 
-echo "[$0] Status of ZMuMu MC reprocessing:" 
-cat mc_all.txt | grep -i ZMuMu
+echo "[$0] Status of Cosmics reprocessing:" 
+cat cosmics_all.txt | grep -v RelVal
+echo
+echo
+
+echo "[$0] Status of ZToMuMu MC reprocessing:" 
+cat mc_all.txt | grep -i ZToMuMu
 echo
 echo
 
@@ -25,12 +31,12 @@ cat mc_all.txt | grep -i DY
 echo
 echo
 
-echo "[$0] Status of ZMuMu JPsi reprocessing:" 
+echo "[$0] Status of JPsi reprocessing:" 
 cat mc_all.txt | grep -i JPsi
 echo
 echo
 
-echo "[$0] Status of ZMuMu MuEnriched reprocessing:" 
+echo "[$0] Status of MuEnriched reprocessing:" 
 cat mc_all.txt | grep -i MuEnriched
 echo
 echo
