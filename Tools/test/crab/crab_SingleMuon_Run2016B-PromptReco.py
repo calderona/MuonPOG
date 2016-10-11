232aaba7313f8060a8a97cde9682a15a78042843
Alicia <calderon@ifca.unican.es>
2016-10-11 11:00:41 +0200

    updated crab files for data

--- Tools/test/crab/crab_SingleMuon_Run2016B-PromptReco.py
+++ Tools/test/crab/crab_SingleMuon_Run2016B-PromptReco.py
@@ -14,8 +14,8 @@ config.JobType.pyCfgParams = ['globalTag=80X_dataRun2_Prompt_v9',
                               'nEvents=-1',
                               'runOnMC=False',
                               'hltPathFilter=all',
-                              'minMuPt=0.0',
-                              'minNMu=0'
+                              'minMuPt=10.0',
+                              'minNMu=2'
                ]
 config.JobType.allowUndistributedCMSSW = True  # To fix cmssw releases
 
